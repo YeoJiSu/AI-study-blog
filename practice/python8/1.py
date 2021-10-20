f9cf96985882081fe5f01bad736a7a8e8750bcf0
YeoJiSu <duwltn1301@naver.com>
2021-10-20 16:51:31 +0900

    Feat : Code about Numpy Matrix # 4

--- practice/python8/1.py
+++ practice/python8/1.py
@@ -9,14 +9,38 @@ a4= np.array([1,2,3],dtype=complex)
 print (a1)
 print (a2)
 print (a3)
-print (a4)
+print (a4,"\n")
 
 #2. numpy- Array creation
-b1=np.arange(10)
+b1=np.arange(12)
 b2=np.arange(2,9,3)
 b3=np.ones(3) #float가 디폴트임 
 b4=np.zeros(6)
+b5=np.linspace(10,20,5)
 print(b1)
 print(b2)
 print(b3)
 print(b4)
+print(b5,"\n")
+
+#3. numpy- reshape
+c1= b1.reshape(3,4)
+print("\nc1= ",c1)
+print(c1[2,1]) # 9를 뜻함
+print(c1[:,1]) # 1,5,9를 뜻함 
+print(np.shape(c1)) #(3,4)의 모양이라는 걸 출력함 dimesion !!
+print(np.size(c1)) # 12
+print(np.ndim(c1),"\n") # 2차원
+
+#4. numpy - Matrix
+m1 = np.eye(3) # identity matrix
+m2 = np.eye(3,4)
+m3 = np.array([[1,2],[3,4]])
+m4 = np.array([[11,12],[13,14]])
+
+print(m1)
+print(m2)
+print(m3+m4)
+print(m3*m4)
+print(np.dot(m3,m4))
+print(np.inner(m3,m4))
