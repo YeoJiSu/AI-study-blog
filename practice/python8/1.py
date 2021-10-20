7ad197bbf11747f2da748885811615d53caa3807
YeoJiSu <duwltn1301@naver.com>
2021-10-20 16:20:35 +0900

    Feat : Code about Numpy arrays

---
+++ practice/python8/1.py
@@ -0,0 +1,12 @@
+#1. numpy- ARRAYS
+import numpy as np
+a= np.array([1,2,3])
+b= np.array([[1,2],[3,4]])
+c= np.array([1,2,3,4,5], ndmin=3)
+# ndmin 차원으로 출력해라 
+d= np.array([1,2,3],dtype=complex)
+# complex -> 복소수 차원으로 출력해라 
+print (a)
+print (b)
+print (c)
+print (d)
