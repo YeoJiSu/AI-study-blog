4d17ca9116429125441836ebdef52ab4f80f5266
YeoJiSu <duwltn1301@naver.com>
2021-12-16 23:21:41 +0900

    delete

--- practice/python6/RandomValue/1.py
+++
@@ -1,7 +0,0 @@
-import random
-mylist= ["jisu","jinyoung","daeuon","mango"]
-print(random.choice(mylist))
-print(random.sample(mylist,3))
-random.shuffle(mylist)
-print(mylist)
-print(random.randint(1,5)) # random integer 1부터 5까지
--- practice/python6/Recursion/1.py
+++
@@ -1,6 +0,0 @@
-def pow(r,n):
-    if n==1:
-        return r
-    else:
-        return r * pow(r,n-1)
-print(pow(2,5))
